af4aef0c5665573c4d2376ae6b0145d7d43c0061
Quan Brew <t@ioover.net>
2018-06-25 17:51:43 +0800

    add the `chrono` back

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 //! Types in the Telegram Bot API and their deserializers
 //!
 //! See also [Telegram Bot API](https://core.telegram.org/bots/api).
-
+#[cfg(feature = "high")]
+extern crate chrono;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
